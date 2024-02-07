16f9f5ce429ea139ffa2176f4a143cc31235166d
Ugur <ugurarslanpeace@gmail.com>
2024-02-07 18:34:40 +0300

    Debug 1

--- src/layouts/main/MainLayout.jsx
+++ src/layouts/main/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar/Sidebar";
-import RightBar from "./Rightbar/Rightbar";
+import RightBar from "./Rightbar/RightBar.jsx";
 
 function MainLayout() {
   return (
